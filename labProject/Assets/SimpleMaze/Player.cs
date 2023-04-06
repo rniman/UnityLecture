16c3283ef8075a7daa9acc576a242750916e4422
rniman <105342620+rniman@users.noreply.github.com>
2023-04-06 21:11:24 +0900

    카메라 회전 구현

--- labProject/Assets/SimpleMaze/Player.cs
+++ labProject/Assets/SimpleMaze/Player.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 2.0f;
-    [SerializeField] private float turnSpeed = 30.0f;
+    [SerializeField] private float turnSpeed = 90.0f;
 
     public GameObject bullet;
 
@@ -78,7 +80,7 @@ public class Player : MonoBehaviour
             Vector3 newBulletPostion = transform.position + transform.forward * 0.7f;
             newBulletPostion.y += 1.0f;
             GameObject newBullet = GameObject.Instantiate(bullet, newBulletPostion, transform.rotation);
-            Destroy(newBullet, 10.0f);
+            Destroy(newBullet, 5.0f);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha1))
@@ -98,13 +100,22 @@ public class Player : MonoBehaviour
             }
         }
 
-        if (Input.GetMouseButton(0))
+        if (!sceneCamera)
+            UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+        else
+            UnityEngine.Cursor.lockState = CursorLockMode.None;
+
+        if (!sceneCamera || Input.GetMouseButton(0)) 
         {
             float horizontal = Input.GetAxis("Mouse X");
             transform.Rotate(Vector3.up * horizontal * turnSpeed * Time.deltaTime);
             if (!sceneCamera)
             {
-                mainCamera.transform.Rotate(Vector3.up * horizontal * turnSpeed * Time.deltaTime);
+                float vertical = -Input.GetAxis("Mouse Y");
+                mainCamera.transform.rotation = Quaternion.Euler(
+                    mainCamera.transform.rotation.eulerAngles.x + vertical * turnSpeed * Time.deltaTime,
+                    mainCamera.transform.rotation.eulerAngles.y + horizontal * turnSpeed * Time.deltaTime,
+                    0.0f);
             }
         }
     }
@@ -114,13 +125,12 @@ public class Player : MonoBehaviour
         if (!usePhysics && moveDirection != Vector3.zero)
         {
             moveDirection = moveDirection.normalized;
-            //rigidbody.MovePosition(transform.position + moveDirection * moveSpeed * Time.deltaTime);
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
             if (!sceneCamera)
             {
                 Vector3 cameraOffset = transform.position;
                 cameraOffset.y += 1.5f;
-                mainCamera.transform.SetPositionAndRotation(cameraOffset, transform.rotation);
+                mainCamera.transform.SetPositionAndRotation(cameraOffset, mainCamera.transform.rotation);
             }
         }
     }
